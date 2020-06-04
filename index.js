648db00546507dfab5e51de8b060d125b64d63a3
Raphael Jansen <raphael.jansen@jusbrasil.com.br>
2020-06-04 19:02:02 -0300

    Update sinon-chrome and add extensions and apps mocks

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ var pattern = function (file) {
 }
 
 var framework = function (files) {
-  var sinonChromePath = path.resolve(require.resolve('sinon-chrome'), '../../dist/sinon-chrome.latest.js')
+  var sinonChromePath = path.resolve(require.resolve('sinon-chrome'), '../bundle/sinon-chrome.min.js')
+  files.unshift(pattern(sinonChromePath))
+}
+
+var frameworkApps = function (files) {
+  var sinonChromePath = path.resolve(require.resolve('sinon-chrome'), '../bundle/sinon-chrome-apps.min.js')
+  files.unshift(pattern(sinonChromePath))
+}
+
+var frameworkExtensions = function (files) {
+  var sinonChromePath = path.resolve(require.resolve('sinon-chrome'), '../bundle/sinon-chrome-webextensions.min.js')
   files.unshift(pattern(sinonChromePath))
 }
 
 framework.$inject = ['config.files']
+frameworkApps.$inject = ['config.files']
+frameworkExtensions.$inject = ['config.files']
 
 module.exports = {
-  'framework:sinon-chrome': ['factory', framework]
+  'framework:sinon-chrome': ['factory', framework],
+  'framework:sinon-chrome-apps': ['factory', frameworkApps],
+  'framework:sinon-chrome-extensions': ['factory', frameworkExtensions]
 }
--- test.js
+++ test.js
@@ -5,18 +5,56 @@
 var fs = require('fs')
 var di = require('di')
 var framework = require('./')
+var frameworkApps = require('./')
+var frameworkExtensions = require('./')
 
-it('should inject files', function () {
-  var config = { 'config': [ 'value', { files: [] } ] }
-  var injector = new di.Injector([ config, framework ])
+describe('sinon-crhome', function () {
+  it('should inject files', function () {
+    var config = { 'config': [ 'value', { files: [] } ] }
+    var injector = new di.Injector([ config, framework ])
 
-  injector.get('framework:sinon-chrome')
+    injector.get('framework:sinon-chrome')
 
-  injector.get('config.files')
-    .map(function (file) {
-      return file.pattern
-    })
-    .forEach(function (path) {
-      fs.accessSync(path)
-    })
+    injector.get('config.files')
+      .map(function (file) {
+        return file.pattern
+      })
+      .forEach(function (path) {
+        fs.accessSync(path)
+      })
+  })
+})
+
+describe('sinon-crhome-apps', function () {
+  it('should inject files', function () {
+    var config = { 'config': [ 'value', { files: [] } ] }
+    var injector = new di.Injector([ config, frameworkApps ])
+
+    injector.get('framework:sinon-chrome-apps')
+
+    injector.get('config.files')
+      .map(function (file) {
+        return file.pattern
+      })
+      .forEach(function (path) {
+        fs.accessSync(path)
+      })
+  })
+})
+
+describe('sinon-crhome-extensions', function () {
+  it('should inject files', function () {
+    var config = { 'config': [ 'value', { files: [] } ] }
+    var injector = new di.Injector([ config, frameworkExtensions ])
+
+    injector.get('framework:sinon-chrome-extensions')
+
+    injector.get('config.files')
+      .map(function (file) {
+        return file.pattern
+      })
+      .forEach(function (path) {
+        fs.accessSync(path)
+      })
+  })
 })
